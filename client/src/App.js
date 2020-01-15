c1463a69e13536bc9dbbce13e16178663d98957c
HarrisonMS <54726103+HarrisonMS@users.noreply.github.com>
2020-01-14 21:15:32 -0800

    state store added

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
+import{useSelector} from 'react-redux';
 
 function App() {
+  const counter = useSelector(state => state.counter)
+  const isLogged = useSelector(state => state.isLogged)
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <h1>counter {counter}</h1>
+      {isLogged}
+      <h3>Valuable information</h3>
     </div>
-  );
+  )
 }
 
 export default App;
